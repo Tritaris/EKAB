a02283557d1db34108bd71725796ae5bba3e775c
Steve Zvosec <szvosec@gmail.com>
2017-11-10 20:23:54 -0500

    Initial commit - Beginning of EKAB

---
+++ EKAB/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UNO {
+    public class BoardManager : MonoBehaviour {
+        Card[] playerCards;
+        Card[] computerCards;
+        Card[] discardedCards;
+        Card[] playDeck;
+        
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ EKAB/Assets/Scripts/CardManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UNO {
+    public class CardManager : MonoBehaviour {
+        BoardManager bm;
+
+        public Sprite[] textures;
+        public string[] names;
+        
+        public Card[] cards;
+
+        // Use this for initialization
+        void Start() {
+            bm = FindObjectOfType<BoardManager>();
+            LoadSprites();
+            GenerateDeck();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            
+        }
+
+        void GenerateDeck() {
+            cards = new Card[108];
+
+
+        }
+
+        void LoadSprites() {
+            textures = Resources.LoadAll<Sprite>("Textures/Cards/");
+            names = new string[textures.Length];
+
+            for (int i = 0; i < names.Length; i++) {
+                names[i] = textures[i].name;
+            }
+        }
+    }
+}

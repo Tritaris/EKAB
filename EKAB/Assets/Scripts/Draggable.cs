4627daf274e631955095ec0e0e05a088701f5d22
Steve Zvosec <szvosec@gmail.com>
2017-11-12 23:49:43 -0500

    Cards are now Drag and Dropable

---
+++ EKAB/Assets/Scripts/Draggable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UNO {
+    public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+        public Transform parentToReturnTo = null;
+
+        public void OnBeginDrag(PointerEventData eventData) {
+            //Debug.Log("OnBeginDrag " + eventData.position);
+            this.transform.position = eventData.position;
+
+            parentToReturnTo = this.transform.parent;
+            this.transform.SetParent(this.transform.parent.parent);
+
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+
+        public void OnDrag(PointerEventData eventData) {
+            //Debug.Log("OnDrag " + eventData.position);
+            this.transform.position = eventData.position;
+        }
+
+        public void OnEndDrag(PointerEventData eventData) {
+            //Debug.Log("OnEndDrag");
+            this.transform.SetParent(parentToReturnTo);
+            this.transform.position = parentToReturnTo.position;
+
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+    }
+}
---
+++ EKAB/Assets/Scripts/DropZone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UNO {
+    public class DropZone : MonoBehaviour, IDropHandler {
+        CardManager cm;
+
+        void Awake() {
+            cm = FindObjectOfType<CardManager>();
+        }
+
+        void IDropHandler.OnDrop(PointerEventData eventData) {
+            
+
+            if (d != null) {
+                if (cm.cardToGameObject.ContainsKey(eventData.pointerDrag)) {
+                    
+                }
+                else {
+                    throw new Exception("cardToGameObject doesn't contain " + eventData.pointerDrag.name);
+                }
+            }
+        }        
+    }
+}

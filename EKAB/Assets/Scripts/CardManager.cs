d3527048533da9c6e5f029fd55eda00b36a36296
Steve Zvosec <szvosec@gmail.com>
2017-11-12 23:47:01 -0500

    Pulled card methods from BoardManager to CardManager Renamed cardList to masterDeck removed BoardManager

--- EKAB/Assets/Scripts/CardManager.cs
+++ EKAB/Assets/Scripts/CardManager.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UNO {
     public class CardManager : MonoBehaviour {
-        public Sprite[] Textures { get; private set; }
-        public string[] Names { get; private set; }        
-        
-        public List<Card> cardList;
+        public List<Card> masterDeck; //Main list of all the cards in game. non-randomized list of cards.
 
-        private BoardManager bm;
+        public List<Card> playDeck;     //The main deck from which card are draw from. Randomized list of cards.
+        public List<Card> discardedCards;      //Contains cards currently out of play.  
+
+        public List<Card> playerCards;
+        public int playerScore;
+        public List<Card> computerCards;
+        public int computerScore;
+
+        public Dictionary<GameObject, Card> cardToGameObject;
+
+        private const int CardsPerHand = 7;
 
         private void Awake() {
-            LoadSprites();
+            cardToGameObject = new Dictionary<GameObject, Card>();
         }
 
-        void Start() {
-            bm = FindObjectOfType<BoardManager>();
-            
+        void Start() {           
             GenerateDeck();
 
             BuildPlayDeck();
+
+            Deal();
         }
         
         void Update() {
@@ -28,7 +36,7 @@ namespace UNO {
         }
 
         void GenerateDeck() {
-            cardList = new List<Card>();            
+            masterDeck = new List<Card>();            
 
             for (int i = 0; i < 2; i++) {
                 foreach (Card.CardColor color in Enum.GetValues(typeof(Card.CardColor))) {
@@ -39,45 +47,70 @@ namespace UNO {
 
                     //Generate 0 Point Cards
                     if (i == 0) {
-                        cardList.Add(new Card(color, Card.CardAction.None, 0));
+                        masterDeck.Add(new Card(color, Card.CardAction.None, 0));
                     }
 
                     //Generate Points Cards
                     for (int j = 1; j <= 9; j++) {                    
-                        cardList.Add(new Card(color, Card.CardAction.None, j));                    
+                        masterDeck.Add(new Card(color, Card.CardAction.None, j));                    
                     }
 
                     //Generate Action Cards                    
-                    cardList.Add(new Card(color, Card.CardAction.DrawTwo, 0));
-                    cardList.Add(new Card(color, Card.CardAction.Reverse, 0));
-                    cardList.Add(new Card(color, Card.CardAction.Skip, 0));                    
+                    masterDeck.Add(new Card(color, Card.CardAction.DrawTwo, 20));
+                    masterDeck.Add(new Card(color, Card.CardAction.Reverse, 20));
+                    masterDeck.Add(new Card(color, Card.CardAction.Skip, 20));                    
                 }
             }
 
             //Generate Wild Cards
             for (int k = 0; k < 4; k++) {
-                cardList.Add(new Card(Card.CardColor.Wild, Card.CardAction.Wild, 0));
-                cardList.Add(new Card(Card.CardColor.Wild, Card.CardAction.WildDraw, 0));
+                masterDeck.Add(new Card(Card.CardColor.Wild, Card.CardAction.Wild, 50));
+                masterDeck.Add(new Card(Card.CardColor.Wild, Card.CardAction.WildDraw, 50));
             }            
         }        
 
-        public void RandomizePlayDeck() {            
-            bm.playDeck.Shuffle(); 
+        public void RandomizeCardList(List<Card> deck) {
+            deck.Shuffle(); 
         }
 
         public void BuildPlayDeck() {
-            bm.playDeck = new List<Card>(cardList);
-            RandomizePlayDeck();
+            playDeck = new List<Card>(masterDeck);
+            RandomizeCardList(playDeck);
         }
 
-        void LoadSprites() {
-            //Loads sprites from resouce folder and assigns them to an array by name
-            Textures = Resources.LoadAll<Sprite>("Textures/Cards/");
-            Names = new string[Textures.Length];
+        void Deal() {
+            //Add cards to hands
+            Draw(playerCards, CardsPerHand);
+            Draw(computerCards, CardsPerHand);
+            Draw(discardedCards, 1);
+        }
 
-            for (int i = 0; i < Names.Length; i++) {
-                Names[i] = Textures[i].name;
+        public void Draw(List<Card> hand, int numberOfCards) {
+            for (int i = 0; i < numberOfCards; i++) {
+                hand.Add(playDeck.ElementAt<Card>(0));
+                playDeck.RemoveAt(0);
             }
+        }
+
+        public void Discard(Card card) {
+            List<Card> tempList;
+            if (playerCards.Contains(card)) {
+                playerScore += card.Points;
+                tempList = playerCards;
+            }
+            else if (computerCards.Contains(card)) {
+                computerScore += card.Points;
+                tempList = computerCards;
+            }
+            else {
+                tempList = discardedCards;
+            }
+
+            int cardIndex = tempList.IndexOf(card);
+            
+            discardedCards.Add(tempList.ElementAt<Card>(cardIndex));
+            tempList.RemoveAt(cardIndex);
+            
         }
     }
 

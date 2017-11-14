e30979f437ef443c5d7b4521f66585fa0194d05b
Steve Zvosec <szvosec@gmail.com>
2017-11-14 01:43:57 -0500

    Modified DropZone for the player/computer's hands Created DiscardPile to handle dropping cards on the discardPanel

---
+++ EKAB/Assets/Scripts/DiscardPile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UNO {
+    public class DiscardPile : MonoBehaviour, IDropHandler {
+        CardManager cm;
+        GameManager gm;
+
+        void Awake() {
+            cm = FindObjectOfType<CardManager>();
+            gm = FindObjectOfType<GameManager>();
+        }
+
+        void IDropHandler.OnDrop(PointerEventData eventData) {
+            Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+            Card c = cm.gameObjectToCard[eventData.pointerDrag];
+            Card topCard = cm.discardedCards[cm.discardedCards.Count - 1];
+
+            if (d != null) {
+                if (cm.gameObjectToCard.ContainsKey(eventData.pointerDrag)) {
+                    //Determine if card can be placed on discard pile
+                    if (c.Color == topCard.Color || c.Color == Card.CardColor.Wild || c.Color == cm.wildCardColor || c.Action == topCard.Action && c.Action != Card.CardAction.None || c.Points == topCard.Points && c.Points < 20) {
+                        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+                        d.parentToReturnTo = this.transform;
+                        d.parentToReturnTo.position = this.transform.position;                        
+                        
+                        gm.ProcessCard(c);
+                        
+                        if(cm.wildCardColor != Card.CardColor.Wild) {
+                            cm.wildCardColor = Card.CardColor.Wild;
+                        }
+                    }
+                }
+                else {
+                    throw new Exception("cardToGameObject doesn't contain " + eventData.pointerDrag.name);
+                }
+            }
+        }        
+    }
+}
--- EKAB/Assets/Scripts/DropZone.cs
+++ EKAB/Assets/Scripts/DropZone.cs
@@ -6,21 +6,33 @@ using UnityEngine.EventSystems;
 namespace UNO {
     public class DropZone : MonoBehaviour, IDropHandler {
         CardManager cm;
+        GameManager gameManager;        
 
         void Awake() {
             cm = FindObjectOfType<CardManager>();
+            gameManager = FindObjectOfType<GameManager>();            
         }
 
         void IDropHandler.OnDrop(PointerEventData eventData) {
+            Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+            Card c = cm.gameObjectToCard[eventData.pointerDrag];
             
 
             if (d != null) {
-                if (cm.cardToGameObject.ContainsKey(eventData.pointerDrag)) {
-                    
-                }
-                else {
-                    throw new Exception("cardToGameObject doesn't contain " + eventData.pointerDrag.name);
-                }
+                if (cm.gameObjectToCard.ContainsKey(eventData.pointerDrag)) {
+                    if (gameObject.name == gameManager.CurrentPlayerName() + "Panel") {
+                        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+                        d.parentToReturnTo = this.transform;
+                        d.parentToReturnTo.position = this.transform.position;
+
+                        foreach (var card in cm.Draw(1)) {
+                            gameManager.CurrentPlayerList().Add(card);
+                        }
+                        //cm.Draw(gameManager.CurrentPlayerList(), 1);
+
+                        gameManager.TogglePlayDeckLock(gameManager.CurrentPlayerList());                        
+                    }
+                }                
             }
         }        
     }
